d6b50bfeed3738991e6ba5a713567cb053d5863c
Anna Blok <Anya14_93@mail.ru>
2022-03-18 17:11:59 +0500

    Delete .DS_Store

--- src/index.js
+++ src/index.js
@@ -1,2 +1,76 @@
-import 'normalize.css'
+import 'normalize.css';
 import './styles/main.scss';
+
+document.getElementById('burgerBtn').addEventListener('click', function () {
+  const menu = document.getElementById('burgerMenu');
+  const isOpen = menu.classList.contains('open');
+
+  this.setAttribute('aria-expanded', !isOpen);
+
+  if (isOpen) {
+    menu.classList.remove('open');
+    menu.setAttribute('hidden', '');
+
+    const activeElements = document.querySelectorAll('.active');
+    activeElements.forEach((element) => {
+      element.classList.remove('active--visible');
+    });
+  } else {
+    menu.classList.add('open');
+    menu.removeAttribute('hidden');
+
+    const activeElements = document.querySelectorAll('.active');
+    activeElements.forEach((element) => {
+      element.classList.add('active--visible');
+    });
+  }
+});
+
+document.querySelectorAll('.has-dropdown').forEach((down) => {
+  down.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const smartphonesMenu = this.querySelector('#smartphones');
+    const gadgetsMenu = this.querySelector('#gadgets');
+
+    this.classList.toggle('open');
+
+    if (smartphonesMenu) {
+      smartphonesMenu.classList.add('show');
+    }
+
+    document.querySelectorAll('.has-dropdown.open').forEach((item) => {
+      if (item !== this) {
+        item.classList.remove('open');
+        item.querySelectorAll('.submenu').forEach((sm) => sm.classList.remove('show'));
+      }
+    });
+  });
+});
+
+document.getElementById('subSmartphones').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const gadgetsMenu = document.querySelector('#gadgets');
+
+  if (gadgetsMenu) {
+    gadgetsMenu.classList.add('show');
+  }
+});
+
+document.getElementById('burgerCatalogBtn').addEventListener('click', function () {
+  const menu = document.querySelector('.burger-menu ul');
+
+  const activeElements = document.querySelectorAll('.active');
+  activeElements.forEach((element) => {
+    element.style.display = 'none';
+  });
+
+  if (window.matchMedia('(max-width: 360px)').matches) {
+    if (menu.style.display === '' || menu.style.display === 'none') {
+      menu.style.display = 'block';
+    } else {
+      menu.style.display = 'none';
+    }
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -2,53 +2,52 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const devServer = (isDev) => !isDev ? {} : {
-    devServer: {
-        open: true,
-        hot: true,
-        port: 8080,
-    }
-};
+// Функция, возвращающая настройки Dev Server, если задан режим разработки
+const devServerConfig = (mode) =>
+  mode === 'development'
+    ? { devServer: { open: true, hot: true, port: 8080 } }
+    : {};
 
-module.exports = ({develop}) => ({
-  mode: develop ? 'development' : 'production',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    clean: true,
-  },
-  plugins: [
-      new HtmlWebpackPlugin({
-          template: './src/index.html'
-      }),
-      new MiniCssExtractPlugin({
-          filename: './styles/main.css'
-      })
-  ],
-  module: {
+module.exports = (env) => {
+  const isDevelopment = env.mode === 'development'; // Используем свойство mode
+
+  return {
+    mode: isDevelopment ? 'development' : 'production',
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'), // Выходная директория
+      filename: 'bundle.js',
+      clean: true,
+    },
+    performance: {
+      hints: false, // Полностью отключаем предупреждения о производительности
+      maxAssetSize: 5 * 1024 * 1024, // Новый предел размера отдельного файла (5 MB)
+      maxEntrypointSize: 5 * 1024 * 1024, // Новый предел размера входной точки (5 MB)
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: './src/index.html' }),
+      new MiniCssExtractPlugin(),
+    ],
+    module: {
       rules: [
-          {
-              test: /\.(?:ico|png|jpg|jpeg|svg)$/i,
-              type: 'asset/inline'
-          },
-          {
-              test: /\.html$/i,
-              loader: 'html-loader'
-          },
-          {
-              test: /\.css$/i,
-              use: [
-                MiniCssExtractPlugin.loader, 'css-loader'
-              ]
-          },
-          {
-              test: /\.scss$/i,
-              use: [
-                MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
-              ]
-          }
-      ]
-  },
-  ...devServer(develop),
-});
+        {
+          test: /\.(?:ico|png|jpg|jpeg|svg)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.html$/i,
+          loader: 'html-loader',
+        },
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.scss$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
+    ...devServerConfig(env.mode),
+  };
+};
